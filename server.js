dbf62ac1ced18fcf1dd6aea104950bb28a29345c
Asad-Kareem <kareembakhs112244@gmail.com>
2023-06-20 13:19:34 -0700

    to the user

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ io.on('connection', (socket) => {
     chat.users.forEach((user) => {
       if (user._id == newMessageRecieved.sender._id) return;
 
-      socket.in(user._id).emit('message recieved', newMessageRecieved);
+      socket.to(user._id).emit('message recieved', newMessageRecieved);
     });
   });
 

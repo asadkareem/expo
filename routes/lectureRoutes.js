20a4c0c3663733645b30ff20a210ceb667f88f42
Irfan Ullah <irfanmhmnd@gmail.com>
2023-10-12 00:12:39 +0500

    update lecture route

--- routes/lectureRoutes.js
+++ routes/lectureRoutes.js
@@ -32,9 +32,39 @@ router.use(authController.protect);
 router.route('/').get(lectureController.getAlllecture);
 router.route('/:id').get(lectureController.getlecture);
 router.use(authController.restrictTo('admin'));
-router
-  .route('/')
-  .post(uploadS3.single('lecture'), lectureController.lectureMiddleware);
+// router
+//   .route('/')
+//   .post(upload.single('lecture'), lectureController.lectureMiddleware);
+  router.route('/').post(async (req, res, next) => {
+
+    try {
+
+
+      // Use the parsefile function to upload the file to S3
+      // console.log(req.body)
+      const uploadedFile = await parsefile(req);
+      
+  
+      // Construct the lectureLink using the uploadedFile data
+      // const lectureLink = `${req.protocol}://${req.get('host')}/api/v1/lectures/getvideo?key=${uploadedFile.Key}`;
+
+      // req.body.lectureLink = uploadedFile.Location;
+  
+      // Create the lecture document
+      const doc = await lecture.create(Object.values(uploadedFile));
+  
+      res.status(201).json({
+        status: 'success',
+        data: doc,
+      });
+    } catch (error) {
+      // Handle any errors that occur during the process
+      res.status(400).json({
+        message: "An error occurred.",
+        error: error,
+      }) // Adjust the status code as needed
+    }
+  });
 router
   .route('/:id')
   .patch(

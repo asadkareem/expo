bb1c6a4c6df7c19b95551f24394e8f72ae4eb617
Sparko-Sol <kareembakhs112244@gmail.com>
2023-10-11 12:15:23 -0700

    lecture-download-change-irfan

--- routes/lectureRoutes.js
+++ routes/lectureRoutes.js
@@ -35,36 +35,36 @@ router.use(authController.restrictTo('admin'));
 // router
 //   .route('/')
 //   .post(upload.single('lecture'), lectureController.lectureMiddleware);
-  router.route('/').post(async (req, res, next) => {
+router.route('/').post(async (req, res, next) => {
 
-    try {
+  try {
 
 
-      // Use the parsefile function to upload the file to S3
-      // console.log(req.body)
-      const uploadedFile = await parsefile(req);
-      
-  
-      // Construct the lectureLink using the uploadedFile data
-      // const lectureLink = `${req.protocol}://${req.get('host')}/api/v1/lectures/getvideo?key=${uploadedFile.Key}`;
+    // Use the parsefile function to upload the file to S3
+    // console.log(req.body)
+    const uploadedFile = await parsefile(req);
 
-      // req.body.lectureLink = uploadedFile.Location;
-  
-      // Create the lecture document
-      const doc = await lecture.create(Object.values(uploadedFile));
-  
-      res.status(201).json({
-        status: 'success',
-        data: doc,
-      });
-    } catch (error) {
-      // Handle any errors that occur during the process
-      res.status(400).json({
-        message: "An error occurred.",
-        error: error,
-      }) // Adjust the status code as needed
-    }
-  });
+
+    // Construct the lectureLink using the uploadedFile data
+    // const lectureLink = `${req.protocol}://${req.get('host')}/api/v1/lectures/getvideo?key=${uploadedFile.Key}`;
+
+    // req.body.lectureLink = uploadedFile.Location;
+
+    // Create the lecture document
+    const doc = await lecture.create(Object.values(uploadedFile));
+
+    res.status(201).json({
+      status: 'success',
+      data: doc,
+    });
+  } catch (error) {
+    // Handle any errors that occur during the process
+    res.status(400).json({
+      message: "An error occurred.",
+      error: error,
+    }) // Adjust the status code as needed
+  }
+});
 router
   .route('/:id')
   .patch(
